36c5db685f09aa66652a7b1b3d083874485269e8
1gilmar <1gilmar@gmail.com>
2023-12-29 12:11:05 -0300

    criado midware

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import ButtonLogout from "@/components/ButtonLogout";
+
+export default function Dashboard() {
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div >
+        seja bem vindo dashbaord
+      </div>
+      <ButtonLogout/>
+    </div>
+  )
+}
