9600a6021748cc29e7121d69203cbdff7c4b4d37
1gilmar <1gilmar@gmail.com>
2024-02-08 22:12:53 -0300

    ajust db

--- src/app/api/user/login/route.ts
+++ src/app/api/user/login/route.ts
@@ -3,8 +3,22 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(request: NextRequest) {
   // const user = users.find((x: { email: string; }) => x.email === email);
   const users = [
-    { email: 'admin@email.com', password: '123123', role: 'adm' },
-    { email: 'user1@email.com', password: '123123', role: 'user' },
+    {
+      id: '1',
+      user: 'ADMIN O1',
+      email: 'admin@email.com',
+      password: '123123',
+      role: 'adm',
+      jwt: 'aassddkklsleie',
+    },
+    {
+      id: '2',
+      user: 'USER 1',
+      email: 'user1@email.com',
+      password: '123123',
+      role: 'user',
+      jwt: 'aassksiwiwwwhwowue',
+    },
   ]
 
   try {
